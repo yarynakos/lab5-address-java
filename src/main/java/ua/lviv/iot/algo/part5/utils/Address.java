a800d930c6a3c1099b051c72dba3039ced2bf07b
Yaryna Kos <117029435+yarynakos@users.noreply.github.com>
2023-05-01 12:50:56 +0300

    fixed lab5

--- src/main/java/ua/lviv/iot/algo/part5/utils/Address.java
+++ src/main/java/ua/lviv/iot/algo/part5/utils/Address.java
@@ -3,12 +3,19 @@ package ua.lviv.iot.algo.part5.utils;
 import java.util.Scanner;
 
 public class Address {
-    public String findAddress() {
+    public String findAddress(String text) {
+        String pattern = "\\w+, str. (\\w+\\s?)+, \\d+\\w?";
+        return text.replaceAll(pattern, "ADDRESS");
+    }
+
+    public static void main(String[] args) {
+        Address address = new Address();
         Scanner scanner = new Scanner(System.in);
         System.out.println("write a text:");
         String text = scanner.nextLine();
+        address.findAddress(text);
+        scanner.close();
         System.out.println("\n");
-        String pattern = "\\w+, str. (\\w+\\s?)+, \\d+\\w?";
-        return text.replaceAll(pattern, "ADDRESS");
+        System.out.println(address.findAddress(text));
     }
 }
--- src/test/java/ua/lviv/iot/algo/part5/utils/AddressTest.java
+++ src/test/java/ua/lviv/iot/algo/part5/utils/AddressTest.java
@@ -1,44 +1,40 @@
 package ua.lviv.iot.algo.part5.utils;
 
-import org.junit.gen5.api.AfterEach;
-import org.junit.gen5.api.BeforeEach;
-import org.junit.gen5.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Scanner;
-
-import static org.junit.gen5.api.Assertions.assertEquals;
 
 class AddressTest {
+    private Address address;
+    private final String expected = "Mr and Mrs Dursley,ADDRESS, were proud to say that they were perfectly normal," +
+            " thank you very much. They were the last people you’d expect to be involved in anything" +
+            " strange or mysterious, because they just didn’t hold with such nonsense.";
+    private final String address1 = "Mr and Mrs Dursley,London, str. Privet Drive, 4 were proud to say that they were perfectly" +
+            " normal, thank you very much. They were the last people you’d expect to be involved in anything" +
+            " strange or mysterious, because they just didn’t hold with such nonsense.";
+    private final String address2 = "Mr and Mrs Dursley, were proud to say that they were perfectly normal, thank you very much." +
+            " They were the last people you’d expect to be involved in anything strange or mysterious," +
+            " because they just didn’t hold with such nonsense.";
+    private final String address3 = "Mr and Mrs Dursley,London, str. Drive, 4 were proud to say that they were perfectly normal," +
+            " thank you very much. They were the last people you’d expect to be involved in anything strange" +
+            " or mysterious, because they just didn’t hold with such nonsense.";
+    private final String address4 = "Mr and Mrs Dursley,London, str. Privet Drive, 4b were proud to say that they were" +
+            " perfectly normal, thank you very much. They were the last people you’d expect to be involved" +
+            " in anything strange or mysterious, because they just didn’t hold with such nonsense.";
 
     @BeforeEach
     void setUp() {
-        Address address = new Address();
-        String expected = "Mr and Mrs Dursley,ADDRESS, were proud to say that they were perfectly normal," +
-                " thank you very much. They were the last people you’d expect to be involved in anything" +
-                " strange or mysterious, because they just didn’t hold with such nonsense.";
-        String address1 = "Mr and Mrs Dursley,London, str. Privet Drive, 4 were proud to say that they were perfectly" +
-                " normal, thank you very much. They were the last people you’d expect to be involved in anything" +
-                " strange or mysterious, because they just didn’t hold with such nonsense.";
-        String address2 = "Mr and Mrs Dursley, were proud to say that they were perfectly normal, thank you very much." +
-                " They were the last people you’d expect to be involved in anything strange or mysterious," +
-                " because they just didn’t hold with such nonsense.";
-        String address3 = "Mr and Mrs Dursley,London, str. Drive, 4 were proud to say that they were perfectly normal," +
-                " thank you very much. They were the last people you’d expect to be involved in anything strange" +
-                " or mysterious, because they just didn’t hold with such nonsense.";
-        String address4 = "Mr and Mrs Dursley,London, str. Privet Drive, 4b were proud to say that they were" +
-                " perfectly normal, thank you very much. They were the last people you’d expect to be involved" +
-                " in anything strange or mysterious, because they just didn’t hold with such nonsense.";
-    }
-
-    @AfterEach
-    void tearDown() {
+        address = new Address();
     }
 
     @Test
     void findAddress() {
-        Scanner in = new Scanner(System.in);
-        String actual = in.nextLine();
-        String a = actual.
+        InputStream inputStream1 = new ByteArrayInputStream(address1.getBytes());
+        Assertions.assertEquals(expected, address.findAddress(expected));
+        address.findAddress(expected);
+        System.setIn(inputStream1);
     }
 }
